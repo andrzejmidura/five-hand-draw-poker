aac75e64a33de74a5fae1d17dc09b20a45077677
andrzejmidura <andrzej.midura253@gmail.com>
2022-03-02 13:56:01 +0100

    Hand class template

---
+++ utils/src/main/java/game/Hand.java
@@ -0,0 +1,24 @@
+package game;
+
+public enum Hand {
+    HIGH_CARD       (1),
+    PAIR            (2),
+    TWO_PAIR        (3),
+    THREE_OF_A_KIND (4),
+    STRAIGHT        (5),
+    FLUSH           (6),
+    FULL_HOUSE      (7),
+    FOUR_OF_A_KIND  (8),
+    STRAIGHT_FLUSH  (9),
+    ROYAL_FLUSH     (10);
+
+    Hand(int val) {
+        this.value = val;
+    }
+
+    public boolean isBetterThan(Hand h) {
+        return this.value>h.value;
+    }
+
+    private final int value;
+    }
