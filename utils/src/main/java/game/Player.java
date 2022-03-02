d1d55955483fb076a6c13590c514467c82702afb
andrzejmidura <andrzej.midura253@gmail.com>
2022-03-02 13:53:40 +0100

    slight refactoring

--- utils/src/main/java/game/Player.java
+++ utils/src/main/java/game/Player.java
@@ -7,24 +7,35 @@ import java.util.List;
 
 public class Player {
     public enum Action {
+        NONE    (0),
         BET     (1),
         FOLD    (2),
         CHECK   (3),
         CALL    (4),
         RAISE   (5),
-        ALLIN   (6);
+        ALLIN   (6),
+        EXIT    (7);
 
         Action(int v) {
             this.value = v;
         }
-        private int value;
+        public final int value;
     }
 
-    private int id;
-    private int pocket = 200;
-    private int stake = 0;
-    private Action currentAction;
-    private List<Card> cards = new ArrayList<>(5);
+    protected String name;
+    protected int pocket;
+    protected int stake = 0;
+    protected Hand hand;
+    protected Action currentAction = Action.NONE;
+    protected List<Card> cards = new ArrayList<>(5);
+
+    public String getName() {
+        return name;
+    }
+
+    public Hand getHand() {
+        return this.hand;
+    }
 
     public int getPocket() {
         return pocket;
@@ -53,8 +64,8 @@ public class Player {
 
 
     public Player() {}
-    public Player(int id) {
-        this.id = id;
+    public Player(String n) {
+        this.name = n;
     }
 
     public void addCard(Card c) {
